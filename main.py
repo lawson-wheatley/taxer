4aa997f4dd528671d1486b3e4854670403647422
lawson-wheatley <30323019+lawson-wheatley@users.noreply.github.com>
2021-11-30 10:47:34 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,48 @@
+import random
+
+
+def randomize(length):
+    """
+    :param lenn:
+    :return list of ages between 0-100:
+    """
+    people = []
+    for i in range(length):
+        people.append(random.randint(0, 100))
+    return people
+
+
+def taxpeople(people):
+    """
+    :param people:
+    :return taxes:
+    """
+    taxer = []
+    for person in people:  # Traverses list
+        taxer.append(tax(person))  # Applies tax
+    return taxer
+
+
+def tax(personage):
+    """
+    :param personage:
+    :return tax:
+    """
+    tax = 0  # No need for check between 0-20
+    if 20 < personage <= 60:  # Checks if person age is between 20 and 60
+        tax = 10
+    elif 60 < personage <= 100:  # Checks if person age is between 60 and 100
+        tax = 5
+    return tax
+
+
+q = input("Do you want a random data set (y/n)? ").lower()
+people = []
+if q == 'y':
+    length = int(input("How many people are you taxing? "))
+    people = randomize(length)
+elif q == 'n':
+    people = list(map(int, input("Please enter in the dataset(ex: 0,2,7,24,63 no spaces): ").split(",")))
+taxes = taxpeople(people)
+for i in range(len(people)):
+    print("Person with age " + str(people[i]) + " has $" + str(taxes[i]) + " in taxes.")
